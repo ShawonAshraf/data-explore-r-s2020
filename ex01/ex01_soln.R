7aed22c6f7e3b6ed19debb4d81c80eb8c88e2a2d
Shawon Ashraf <shawon13@live.com>
2020-05-13 18:23:36 +0600

    ex_01_soln beginnt

---
+++ ex01/ex01_soln.R
@@ -0,0 +1,13 @@
+# creating vectors
+
+abschminken <- c(35,13,88,75,5,66)
+anfressen <- c(1,53,0,0,23,2)
+aufschaukeln <- c(28,3,0,18,1,23)
+nachkochen <- c(0,250,15,77,24)
+fressen <- c(0,99,2,44,38,22)
+kochen <- c(0,26,0,12,86,8)
+schaukeln <- c(68,7,11,31,2,24)
+schminken <- c(41,3,38,4,0,5)
+
+# normalizing
+
