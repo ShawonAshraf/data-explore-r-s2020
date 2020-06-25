5795ae94ead8cd1c9c81f59437ff15d7ca05a189
Shawon Ashraf <shawon13@live.com>
2020-06-26 01:52:05 +0600

    hypo 1

--- project/var_target_corr.R
+++ project/var_target_corr.R
@@ -1,3 +1,4 @@
+# hypothesis 2 
 ## Norms to freq correlation
 
 # load paccman
@@ -20,3 +21,22 @@ concm <- correlate(norms_f.ratings$Conc.M, target.ratings$freq)
 sd <- correlate(norms_f.ratings$Conc.SD, target.ratings$freq)
 pk <- correlate(norms_f.ratings$Percent_known, target.ratings$freq)
 
+# Hypothesis 1
+
+# sum up 
+pacman::p_load(plyr)
+
+? ddply
+
+win.mean <- ddply(windows, "target", numcolwise(mean))
+norm.sub <- subset(norms_f.ratings, Word %in% win.mean$target)
+win.mean <- subset(win.mean, target %in% norm.sub$Word)
+
+correlate(win.mean$freq, norm.sub$Conc.M)
+correlate(win.mean$lmi, norm.sub$Conc.M)
+
+correlate(win.mean$freq, norm.sub$Conc.SD)
+correlate(win.mean$lmi, norm.sub$Conc.SD)
+
+correlate(win.mean$freq, norm.sub$Percent_known)
+correlate(win.mean$lmi, norm.sub$Percent_known)
